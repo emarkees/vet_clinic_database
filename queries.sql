2bea0f5e3f5e11b57512787162f66e91dc8768f7
emarkees <rphillipse@gmail.com>
2023-06-28 14:26:33 -0600

    Add query data to queries.sql files

--- queries.sql
+++ queries.sql
@@ -8,3 +8,27 @@ SELECT name, date_of_birth FROM animals WHERE weight_kg >= '10.5';
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Inside a transaction update the animals table by setting the species column to unspecified */
+BEGIN;
+
+UPDATE animals SET species = 'unspecified';
+
+ROLLBACK;
+
+COMMIT;
+
+/* Update the animals table by setting the species column to digimon for all animals that have a name ending in mon */
+
+BEGIN;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+/* To verify changes */
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+COMMIT;
