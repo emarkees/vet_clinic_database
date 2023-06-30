3b21191133157fe3cda5639a5d2ec94f6ff6c7a4
emarkees <rphillipse@gmail.com>
2023-06-30 12:29:25 -0600

    Add schema.sql files

--- schema.sql
+++ schema.sql
@@ -10,3 +10,35 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+
+/* Schema for owners table */
+
+CREATE TABLE owners (
+  id SERIAL NOT NULL PRIMARY KEY,
+  full_name VARCHAR(255) NOT NULL,
+  age INTEGER NOT NULL
+);
+
+/* Schema for species table */
+
+CREATE TABLE species (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+/* modify animal table */
+
+ALTER TABLE animals
+ALTER COLUMN id SET DATA TYPE SERIAL PRIMARY KEY;
+
+/* add species_id and owners_id for rerfrencing */
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER,
+ADD COLUMN owner_id INTEGER,
+ADD CONSTRAINT fk_species
+  FOREIGN KEY (species_id)
+  REFERENCES species(id),
+ADD CONSTRAINT fk_owners
+  FOREIGN KEY (owner_id)
+  REFERENCES owners(id);
